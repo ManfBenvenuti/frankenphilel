7bc4c0c75ec00103380398772fa34d3a14ea56b2
Matteo Balzarini <aptero@inboxia.com>
2015-02-15 22:17:25 +0000

    fix css

--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,6 @@
 
 ActiveRecord::Schema.define(version: 20150211151014) do
 
-  create_table "ospites", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "ospites", ["email"], name: "index_ospites_on_email", unique: true
-  add_index "ospites", ["reset_password_token"], name: "index_ospites_on_reset_password_token", unique: true
-
   create_table "utente_ospites", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
