5316551605f97705a9a9bf4e317a4b39219a08ba
Matteo Balzarini <aptero@inboxia.com>
2015-02-22 21:40:39 +0000

    PUSH DI BASE

--- app/assets/javascripts/startup_framework.js
+++ app/assets/javascripts/startup_framework.js
@@ -1,4 +1,5 @@
 
+//= require startup-framework/common-files/js/modernizr.custom
 //= require startup-framework/common-files/js/page-transitions
 //= require startup-framework/common-files/js/jquery.scrollTo-1.4.3.1-min
 //= require startup-framework/common-files/js/jquery.parallax.min
