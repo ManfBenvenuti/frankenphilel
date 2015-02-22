acccaabae1e9eb5c53bd3269709ad47aba7e8902
Matteo Balzarini <aptero@inboxia.com>
2015-02-22 21:07:25 +0000

    Revert 5944b05..c8c943e
    This rolls back to commit 5944b053a185313083cd84fec300417ce5caad4b.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -256,4 +256,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  config.secret_key = 'a93236a53bc6611fc8aede281543f11874b36febf8eafd53b44171921ba0251abecd62c70b1b93c1e5c6a0baabdcccb68339837cee19d3710daab7fff29a128e'
 end
